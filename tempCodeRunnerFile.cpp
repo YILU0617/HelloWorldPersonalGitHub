93c2342fee1ddaa0a8857979e936a55127cb12f7
YILU0617 <ylu3@zagmail.gonzaga.edu>
2022-09-20 22:04:17 -0700

    DA1

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<string> v1 {"Hello ", "C++ ", "World ", "from ",
+"YI"};
+for(int i=0; i< v1.size(); i++){
+cout << v1[i];
+}
+
+return 0;
+}
